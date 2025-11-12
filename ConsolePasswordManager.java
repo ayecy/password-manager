e33a3dfcb1be9e476e194b8b599cba6bcf0ba5a3
laplas87 <butinsamat23@gmail.com>
2025-11-12 23:06:20 +0700

    Добавление методов шифрования и дешифрования паролей

--- ConsolePasswordManager.java
+++ ConsolePasswordManager.java
@@ -1,3 +1,6 @@
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
 import java.util.*;
 import java.io.*;
 
@@ -72,6 +75,20 @@ public class ConsolePasswordManager {
     private static void savePasswordsToFile() {
 
     }
-
+    private static String encrypt(String plainText, String secret) throws Exception {
+        SecretKeySpec keySpec = new SecretKeySpec(secret.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+        byte[] encryptedBytes = cipher.doFinal(plainText.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+    private static String decrypt(String encryptedText, String secret) throws Exception {
+        SecretKeySpec keySpec = new SecretKeySpec(secret.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, keySpec);
+        byte[] decodedBytes = Base64.getDecoder().decode(encryptedText);
+        byte[] decryptedBytes = cipher.doFinal(decodedBytes);
+        return new String(decryptedBytes);
+    }
 
 }
